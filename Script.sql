d9615dd079b3d14c19f809e4db956abb0c89f460
Renê Zanette <renezanette@hotmail.com>
2021-06-30 22:48:13 -0300

    Adicionada a tabela cep e cidade

--- Script.sql
+++ Script.sql
@@ -1,4 +1,4 @@
---Criação da tabela empresa
+--Criacao da tabela empresa
 create table empresa (
 	cnpjemp numeric(14) constraint empresa_pk primary key,
 	nomfanemp varchar(40) not null,
@@ -9,4 +9,14 @@ create table empresa (
 	telcontemp numeric(20) not null,
 	tipoemp char(1) not null
 );
-.
+
+CREATE TABLE cep (
+	cep integer NOT NULL CONSTRAINT cep_pk PRIMARY KEY, 
+	codCid integer
+);
+
+CREATE TABLE cidade (
+	codCid integer NOT NULL,
+	nomcid varchar(255) NOT NULL,
+	codEst
+);
