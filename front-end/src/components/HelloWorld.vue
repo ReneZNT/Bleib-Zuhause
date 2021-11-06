98cecb589caa1c78568f787c82f1979f14d63910
ReneZNT <36896137+ReneZNT@users.noreply.github.com>
2021-11-06 11:26:33 -0300

    0.1 Adicionado o Front-End Vue

---
+++ front-end/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    Componente
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+     //
+    }),
+  }
+</script>
---
+++ front-end/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container class="about">
+    <h1>Sobre</h1>
+  </v-container>
+</template>
